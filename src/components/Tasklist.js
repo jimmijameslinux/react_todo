3f22b2d26724401a219abe5b0002dd526e23a136
Jimmi James <133916219+jimmijameslinux@users.noreply.github.com>
2024-06-09 17:38:59 +0000

    Adding dummy functions for del, edit ,complete task

--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -1,13 +1,25 @@
 import "../components/Tasklist.css";
 
+const editTask = () => {
+  console.log("edit task");
+};
+
+const deleteTask = () => {
+  console.log("delete task");
+};
+
+const completeTask = () => {
+  console.log("complete task");
+};
+
 function Tasklist({ tasklist }) {
   return (
     <div className="tasklists">
       <span>{tasklist}</span>
       <div className="btns">
-        <button>Edit</button>
-        <button>Delete</button>
-        <button>Complete</button>
+        <button onClick={editTask}>Edit</button>
+        <button onClick={deleteTask}>Delete</button>
+        <button onClick={completeTask}>Complete</button>
       </div>
     </div>
   );
